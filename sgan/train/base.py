0d8f5ad22f086e7f511d8f4b84723b5bb0a2cc72
stanislaushimovolos <shimovolos.sa@phystech.edu>
2020-11-25 23:54:08 +0300

    first attempt to remove solid code

--- sgan/train/base.py
+++ sgan/train/base.py
@@ -1,1 +1,31 @@
-# TODO add utils, checkpoints, callbacks, tensorboard, etc
+import torch
+from torch import nn
+
+from typing import Callable, Sequence
+
+
+def get_device(x):
+    if isinstance(x, nn.Module):
+        try:
+            return next(x.parameters()).device
+        except StopIteration:
+            raise ValueError('No parameters inside module')
+    elif isinstance(x, torch.Tensor):
+        return x.device
+    else:
+        raise RuntimeError('Wrong input')
+
+
+def to_numpy(x: torch.Tensor):
+    return x.data.cpu().numpy()
+
+
+def process_batch(inputs: torch.Tensor, target: torch.Tensor, model: nn.Module,
+                  criterion: Callable):
+    model.train()
+    inputs = inputs.to(device=get_device(model))
+    target = target.to(device=get_device(model))
+
+    loss = criterion(model(inputs), target)
+    loss.backward()
+    return to_numpy(loss)
