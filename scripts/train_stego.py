7de2e77f3ea579fec178ce9bf06d5646e0bcd671
YanLo <logovoya@tut.by>
2020-12-07 23:03:34 +0300

    add train on synth images

--- scripts/train_stego.py
+++ scripts/train_stego.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import numpy as np
 import sklearn
+import json
 
 from tqdm import tqdm
 from dpipe.io import save_numpy
@@ -21,13 +22,42 @@ def run_experiment(*, device, download: bool, n_epoch: int, batch_size: int, n_n
     # path to save everything related to experiment
     data_path = Path(data_path).expanduser()
     experiment_path = Path(experiment_path).expanduser()
-    # dataset and batch iterator
+    # dataset and batch iterator: real images
     dataset = CelebDataset(root=data_path, download=download)
-    train_indices, val_indices, test_indices = split_data(dataset, train_size=0.6, val_size=0.2, test_size=0.2)
+    train_indices, val_indices, test_indices = split_data(dataset, train_size=0.6
+                                                          , val_size=0.2, test_size=0.2)
+    with open(experiment_path / 'test_indices.json', 'w') as file:
+        json.dump(test_indices, file)
     train_iterator = BatchIterator(dataset, train_indices, batch_size=batch_size)
     val_iterator = BatchIterator(dataset, val_indices, batch_size=batch_size)
-    # TODO: save test_indices
-    test_iterator = BatchIterator(dataset, test_indices, batch_size=batch_size)
+    # trained generators: from dcgan and sgan
+    generator_dcgan = Generator().to(device)
+    generator_dcgan.load_state_dict(torch.load(dcgan_experiment_path
+                                               / f'generator/generator_{generator_load_epoch}'))
+    generator_sgan = Generator().to(device)
+    generator_sgan.load_state_dict(torch.load(sgan_experiment_path
+                                              / f'generator/generator_{generator_load_epoch}'))
+    # dataset and batch iterator: synth images
+    # dcgan
+    generated_images_dcgan = generator_dcgan(generate_noise(len(dataset), n_noise_channels, device))
+    train_indices_dcgan, val_indices_dcgan, test_indices_dcgan = split_data(generated_images_dcgan
+                                                          , train_size=0.6, val_size=0.2, test_size=0.2)
+    with open(experiment_path / 'test_indices_dcgan.json', 'w') as file:
+        json.dump(test_indices_dcgan, file)
+    train_iterator_dcgan = BatchIterator(generated_images_dcgan, train_indices_dcgan
+                                         , batch_size=batch_size)
+    val_iterator_dcgan = BatchIterator(generated_images_dcgan, val_indices_dcgan
+                                       , batch_size=batch_size)
+    # sgan
+    generated_images_sgan = generator_sgan(generate_noise(len(dataset), n_noise_channels, device))
+    train_indices_sgan, val_indices_sgan, test_indices_sgan = split_data(generated_images_sgan
+                                                        , train_size=0.6, val_size=0.2, test_size=0.2)
+    with open(experiment_path / 'test_indices_sgan.json', 'w') as file:
+        json.dump(test_indices_sgan, file)
+    train_iterator_sgan = BatchIterator(generated_images_sgan, train_indices_sgan
+                                         , batch_size=batch_size)
+    val_iterator_sgan = BatchIterator(generated_images_sgan, val_indices_sgan
+                                       , batch_size=batch_size)
     # text loader and encoder
     encoder = SigmoidTorchEncoder(beta=embedding_fidelity)
     text_loader = TextLoader()
@@ -35,18 +65,9 @@ def run_experiment(*, device, download: bool, n_epoch: int, batch_size: int, n_n
     # stegoanalyser: distinguish image with message from just image
     stegoanalyser = Discriminator().to(device)
     stegoanalyser.apply(init_weights)
-    # trained generators: from dcgan and sgan
-    # generator_dcgan = Generator().to(device)
-    # generator_dcgan.load_state_dict(torch.load(dcgan_experiment_path
-    #                                             / f'generator/generator_{generator_load_epoch}'))
-    # generator_sgan = Generator().to(device)
-    # generator_sgan.load_state_dict(torch.load(sgan_experiment_path
-    #                                           / f'generator/generator_{generator_load_epoch}'))
-
     optimizer_parameters = dict(lr=1e-4, betas=(0.5, 0.99))
     stegoanalyser_opt = Adam(stegoanalyser.parameters(), **optimizer_parameters)
 
-    fixed_noise = torch.randn(64, n_noise_channels, 1, 1, device=device)
 
     def save_models(epoch):
         os.makedirs(experiment_path / f'stegoanalyser', exist_ok=True)
@@ -56,11 +77,10 @@ def run_experiment(*, device, download: bool, n_epoch: int, batch_size: int, n_n
     epoch_callbacks = [save_models]
 
     # train on real images: with/without messages
-    train_on_real_img(
+    train_stego(
         stegoanalyser=stegoanalyser,
         train_iterator=train_iterator,
         val_iterator=val_iterator,
-        test_iterator = test_iterator,
         text_iterator=text_iterator,
         n_epoch=n_epoch,
         stegoanalyser_opt=stegoanalyser_opt,
@@ -69,10 +89,34 @@ def run_experiment(*, device, download: bool, n_epoch: int, batch_size: int, n_n
         encoder=encoder
     )
     # train on generator_dcgan synth images: with/without messages
+    train_stego(
+        stegoanalyser=stegoanalyser,
+        train_iterator=train_iterator_dcgan,
+        val_iterator=val_iterator_dcgan,
+        text_iterator=text_iterator,
+        n_epoch=n_epoch,
+        stegoanalyser_opt=stegoanalyser_opt,
+        callbacks=epoch_callbacks,
+        logger=logger,
+        encoder=encoder
+    )
     # train on generator_sgan synth images: with/withput messages
+    train_stego(
+        stegoanalyser=stegoanalyser,
+        train_iterator=train_iterator_sgan,
+        val_iterator=val_iterator_sgan,
+        text_iterator=text_iterator,
+        n_epoch=n_epoch,
+        stegoanalyser_opt=stegoanalyser_opt,
+        callbacks=epoch_callbacks,
+        logger=logger,
+        encoder=encoder
+    )
 
-def train_on_real_img(*, stegoanalyser: nn.Module, train_iterator: BatchIterator, val_iterator: BatchIterator
-                      , test_iterator: BatchIterator, text_iterator: Iterator
+def train_stego(*, stegoanalyser: nn.Module
+                      , train_iterator: BatchIterator
+                      , val_iterator: BatchIterator
+                      , text_iterator: Iterator
                       , n_epoch: int, stegoanalyser_opt: Optimizer
                       , callbacks: Sequence[Callable] = None, logger: TBLogger
                       , encoder: SigmoidTorchEncoder):
@@ -137,6 +181,8 @@ def train_on_real_img(*, stegoanalyser: nn.Module, train_iterator: BatchIterator
 
             losses = {'Stegoanalyser loss': np.mean(stegoanalyser_losses)}
             logger.policies(losses, epoch)
+
+
 
 def main():
     parser = argparse.ArgumentParser()
